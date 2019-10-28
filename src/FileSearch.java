18549cd364909ac3c521332b86eb8894e4d6d798
Nelson Nyland <nelsonnyland@gmail.com>
2019-10-27 18:56:40 -0700

    Updated search methods.

--- src/FileSearch.java
+++ src/FileSearch.java
@@ -1,51 +1,66 @@
-import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.File;
-import java.io.FilenameFilter;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.PathMatcher;
-import java.util.Scanner;
+//import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
 
 // FileSearch 0.1
 // This application searches a directory for files and folders
 // and returns the location if it matches the search term.
 public class FileSearch {
+
+    private static SearchItem SearchItem;
+
     public static void main(String[] args) {
         // get input
-        SearchItem searchItem = input();
+        input();
         // search for item
-        File[] matchingFiles = search(searchItem);
+        search();
         // format search results
-        //format();
+        format();
         // output results to console
-        output(matchingFiles);
+        output();
     }
 
-    private static SearchItem input() {
-        SearchItem searchItem = new SearchItem();
+    private static void input() {
+        SearchItem = new SearchItem();
         Scanner scanner = new Scanner(System.in);
         System.out.println("FileSearch 0.1");
         System.out.print("Enter search: ");
-        searchItem.item = scanner.nextLine();
+        SearchItem.Query = scanner.nextLine();
         System.out.print("Enter directory: ");
-        searchItem.directory = scanner.nextLine();
-        return searchItem;
+        SearchItem.Directory = scanner.nextLine();
     }
 
-    private static File[] search(SearchItem searchItem) {
-        //PathMatcher matcher =
-        //    FileSystems.getDefault().getPathMatcher("glob:*");
-        File file = new File(searchItem.directory);
-        return file.listFiles((dir, name) -> name.contains(searchItem.item));
+    private static void search() {
+        File file = new File(SearchItem.Directory);
+        //PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:*");
+        //searchItem.result = file.listFiles((dir, name) -> name.contains(searchItem.item));
+        FilenameFilter filter = (dir, name) -> name.contains(SearchItem.Query);
+        SearchItem.FileName = file.list(filter);
     }
 
-    public static void format() {
-        //
-    }
-
-    public static void output(File[] matchingFiles) {
-        for (File file : matchingFiles) {
-            System.out.println(file);
+    private static void format() {
+        SearchItem.Path = new String[SearchItem.FileName.length];
+        for (int i = 0; i < SearchItem.FileName.length; i++) {
+             File file = new File(SearchItem.FileName[i]);
+             SearchItem.Path[i] = file.getAbsolutePath();
         }
     }
+
+    private static void output() {
+        if (SearchItem.FileName.length == 0) {
+            System.out.println("NO FILES FOUND");
+        } else {
+            for (String path : SearchItem.Path) {
+                System.out.println(path);
+            }
+        }
+    }
+}
+
+class SearchItem {
+    String Query;
+    String Directory;
+    String[] FileName;
+    String[] Path;
 }
