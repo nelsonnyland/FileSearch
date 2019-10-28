3e91dd47f9020df7172d92697878b3b19ce8cea9
Nelson Nyland <nelsonnyland@gmail.com>
2019-10-28 13:50:29 -0700

    Code cleanup.

--- src/FileSearch.java
+++ src/FileSearch.java
@@ -13,20 +13,13 @@ public class FileSearch {
     private static int ERROR_COUNT = 0;
 
     public static void main(String[] args) throws IOException {
-        // get input
-        input();
-        // check search mode
-        mode();
-        // search for item
-        //File rootDir = new File(SearchItem.Directory);
-        search(SearchItem.Mode);
-        // format search results
-        //format();
-        // output results to console
-        //output();
+        getConsoleInput();
+        setSearchMode();
+        checkSearchMode();
+        search();
     }
 
-    private static void input() {
+    private static void getConsoleInput() {
         SearchItem = new SearchItem();
         Scanner scanner = new Scanner(System.in);
         System.out.println("FileSearch 0.1");
@@ -38,7 +31,7 @@ public class FileSearch {
         SearchItem.Directory = scanner.nextLine();
     }
 
-    private static void mode() {
+    private static void setSearchMode() {
         if (SearchItem.SearchMode.toLowerCase().charAt(0) == 'y') {
             SearchItem.Mode = 0; // safe search
         } else {
@@ -46,46 +39,16 @@ public class FileSearch {
         }
     }
 
-    private static void search(int mode) throws IOException {
+    private static void checkSearchMode() {
         if (SearchItem.Mode == 0) {
             long startTimer = System.currentTimeMillis();
             SearchItem.StopTimer = startTimer + 60 * 1000; // 60 seconds * 1000 ms/sec
-            search();
-        } else {
-            search();
         }
     }
 
     // recursively searches subdirectories
     private static void search() throws IOException {
-        //PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:*");
-        //searchItem.result = rootDir.listFiles((dir, name) -> name.contains(searchItem.item));
-        //FilenameFilter filter = (dir, name) -> name.contains(SearchItem.Query);
-        //SearchItem.FileName = rootDir.list(filter);
         try {
-            //if (rootDir.listFiles() != null) {
-            // push new dir to stack
-            //SearchItem.Directories.push(rootDir.listFiles());
-            // go through the files in the dir
-            //for (int i = 0; i < SearchItem.Directories.peek().length; i++) {
-            //if (SearchItem.Directories.peek()[i] != null) {
-            //if (SearchItem.Directories.peek()[i].isFile()) {
-            // if filename contains search phrase, add to FoundPaths
-            //if (SearchItem.Directories.peek()[i].getName().toLowerCase().contains(SearchItem.Query.toLowerCase())) {
-            //SearchItem.FoundPaths.add(new File (SearchItem.Directories.peek()[i].getAbsolutePath()));
-            //} // else recursively crawl through subdirectories
-            //} else if (SearchItem.Directories.peek()[i].isDirectory()) {
-            //search(SearchItem.Directories.peek()[i]);
-            //}
-            //}
-            //}
-            //}
-            //Files.walk(Paths.get(SearchItem.Directory)).filter(Files::isRegularFile).forEach((f) -> {
-            //String file = f.toString().toLowerCase();
-            //if (file.contains(SearchItem.Query.toLowerCase())) {
-            //SearchItem.FoundPaths.add(f.toFile());
-            //}
-            //});
             File dir = new File(SearchItem.Directory);
             Files.walkFileTree(
                     Paths.get(dir.getPath()),
@@ -126,24 +89,6 @@ public class FileSearch {
             }
         }
     }
-
-    //private static void format() {
-        //SearchItem.Path = new String[SearchItem.FileName.length];
-        //for (int i = 0; i < SearchItem.FileName.length; i++) {
-        //     File file = new File(SearchItem.FileName[i]);
-        //     SearchItem.Path[i] = file.getAbsolutePath();
-        //}
-    //}
-
-    //private static void output() {
-        //if (SearchItem.FoundPaths.size() == 0) {
-            //System.out.println("NO FILES OR FOLDERS FOUND");
-        //} else {
-            //for (File path : SearchItem.FoundPaths) {
-                //System.out.println(path);
-            //}
-        //}
-    //}
 }
 
 class SearchItem {
@@ -152,8 +97,6 @@ class SearchItem {
     String SearchMode;
     int Mode;
     long StopTimer;
-    String[] FileName;
-    String[] Path;
     Stack<File[]> Directories;
     List<File> FoundPaths;
 
